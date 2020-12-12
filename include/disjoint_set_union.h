9dd117a313db41f4224d9380f19184b26364e728
Margarita <bogatova.md@gmail.com>
2020-12-12 15:45:20 +0300

    DSU implementation

--- include/disjoint_set_union.h
+++ include/disjoint_set_union.h
@@ -5,14 +5,26 @@
 
 class DisjointSetUnion {
 private:
-    bool is_path_compression_performed;
 
-    // parent[i] = index parent of i if (i in U) else 0
-    // parent[i] = i -> i root
-    std::vector<uint64_t> parent;
+    std::vector<std::size_t> rank;
+protected:
 
-    std::vector<uint64_t> rang;
+// parent[i] = index parent of i if (i in U) else 0
+// parent[i] = i -> i root
+    std::vector<std::size_t> parent;
 
+    const static std::size_t NON_EXISTENT_ELEMENT = std::numeric_limits<std::size_t>::max();
+public:
+    void create(std::size_t v);
+
+    std::size_t findSet(std::size_t v);
+
+    void unionSets(std::size_t x, std::size_t y);
+};
+
+class DisjointSetUnionWithPathCompression : public DisjointSetUnion {
+public:
+    std::size_t findSet(std::size_t v);
 };
 
 
--- src/disjoint_set_union.cpp
+++ src/disjoint_set_union.cpp
@@ -1,1 +1,39 @@
 #include "../include/disjoint_set_union.h"
+
+void DisjointSetUnion::create(std::size_t v) {
+    parent[v] = v;
+    rank[v] = 0;
+}
+
+std::size_t DisjointSetUnion::findSet(std::size_t v) {
+    if (v == parent[v]) {
+        return v;
+    }
+    if (parent[v] == 0) {
+        return NON_EXISTENT_ELEMENT;
+    }
+    return findSet(parent[v]);
+}
+
+void DisjointSetUnion::unionSets(std::size_t x, std::size_t y) {
+    if (x != y) {
+        if (rank[x] > rank[y]) {
+            parent[y] = x;
+        } else if (rank[x] < rank[y]) {
+            parent[x] = y;
+        } else {
+            parent[y] = x;
+            rank[x]++;
+        }
+    }
+}
+
+std::size_t DisjointSetUnionWithPathCompression::findSet(std::size_t v) {
+    if (v == parent[v]) {
+        return v;
+    }
+    if (parent[v] == 0) {
+        return NON_EXISTENT_ELEMENT;
+    }
+    return parent[v] = findSet(parent[v]);
+}
